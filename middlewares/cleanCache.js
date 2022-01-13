5476bcaeeab7d408079d2d2d2fc4069cb35924df
yeshaq1 <yousef.eshaq@mail.mcgill.ca>
2022-01-13 17:23:47 -0500

    adding testing with jest

--- middlewares/cleanCache.js
+++ middlewares/cleanCache.js
@@ -2,6 +2,6 @@ const { clearHash } = require("../services/cache");
 
 module.exports = async (req, res, next) => {
   await next();
-
+  // great way to trigger middelware after request handler is complete
   clearHash(req.user.id);
 };
